e55ad625d7d011ea6167e6472348c4a2ff177399
Nzheinenm <koliasam1@gmail.com>
2024-06-10 21:25:28 +0300

    init project

---
+++ src/main/java/ru/pet/nzcheinenm/NzcheinenmApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.nzcheinenm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzcheinenmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NzcheinenmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pet/nzcheinenm/NzcheinenmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.nzcheinenm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzcheinenmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
