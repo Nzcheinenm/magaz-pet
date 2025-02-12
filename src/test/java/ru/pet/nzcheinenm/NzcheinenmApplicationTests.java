43490a5602f4357b95d15e40beb656fbf33cee55
Dmitry <koliasam1@gmail.com>
2025-02-12 19:44:48 +0300

    add test-containers and config test

--- src/test/java/ru/pet/nzcheinenm/NzcheinenmApplicationTests.java
+++ src/test/java/ru/pet/nzcheinenm/NzcheinenmApplicationTests.java
@@ -1,13 +1,57 @@
 package ru.pet.nzcheinenm;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import ru.pet.nzcheinenm.repository.ReactiveProductRepository;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(initializers = {NzcheinenmApplicationTests.Initializer.class})
+@ActiveProfiles("test")
 class NzcheinenmApplicationTests {
 
+    private static int POSTGRES_PORT = 5432;
+
+    @Autowired
+    private ReactiveProductRepository fooRepository;
+
+    @ClassRule
+    public static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres")
+            .withDatabaseName("foo")
+            .withUsername("it_user")
+            .withPassword("it_pass")
+            .withInitScript("sql/init_postgres.sql");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.data.postgres.host=" + postgres.getContainerIpAddress(),
+                    "spring.data.postgres.port=" + postgres.getMappedPort(POSTGRES_PORT),
+                    "spring.data.postgres.username=" + postgres.getUsername(),
+                    "spring.data.postgres.password=" + postgres.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
     @Test
-    void contextLoads() {
+    public void testApp() {
     }
 
 }
+
+
+
+
---
+++ src/test/java/ru/pet/nzcheinenm/config/EmbeddedPostgresConfiguration.java
@@ -0,0 +1,11 @@
+package ru.pet.nzcheinenm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class EmbeddedPostgresConfiguration {
+
+}
