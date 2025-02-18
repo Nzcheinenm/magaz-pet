d2a44afb42b6e7cf7129cac1241c95f2e39f3681
Dmitry <koliasam1@gmail.com>
2025-02-18 21:07:09 +0300

    add kafka testcontainer+ init database testcontainer

--- src/test/java/ru/pet/nzcheinenm/NzcheinenmApplicationTests.java
+++ src/test/java/ru/pet/nzcheinenm/NzcheinenmApplicationTests.java
@@ -1,53 +1,79 @@
 package ru.pet.nzcheinenm;
 
 import org.junit.ClassRule;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+import ru.pet.nzcheinenm.entity.Product;
 import ru.pet.nzcheinenm.repository.ReactiveProductRepository;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
 @SpringBootTest
-@ContextConfiguration(initializers = {NzcheinenmApplicationTests.Initializer.class})
+@Testcontainers
 @ActiveProfiles("test")
 class NzcheinenmApplicationTests {
 
-    private static int POSTGRES_PORT = 5432;
+    @Autowired
+    ReactiveProductRepository repository;
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @Container
+    private static final KafkaContainer kafka = new KafkaContainer(
+            "apache/kafka-native:3.8.0"
+    );
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
 
     @Autowired
-    private ReactiveProductRepository fooRepository;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
-    @ClassRule
-    public static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres")
-            .withDatabaseName("foo")
-            .withUsername("it_user")
-            .withPassword("it_pass")
-            .withInitScript("sql/init_postgres.sql");
+    @DynamicPropertySource
+    static void dynamicPropertyRegistry(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", NzcheinenmApplicationTests::getR2dbcUrl);
+        registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername);
+        registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword);
+    }
 
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.data.postgres.host=" + postgres.getContainerIpAddress(),
-                    "spring.data.postgres.port=" + postgres.getMappedPort(POSTGRES_PORT),
-                    "spring.data.postgres.username=" + postgres.getUsername(),
-                    "spring.data.postgres.password=" + postgres.getPassword()
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
+    private static String getR2dbcUrl() {
+        return postgreSQLContainer.getJdbcUrl().replace("jdbc", "r2dbc");
     }
 
     @Test
-    public void testApp() {
+    void contextLoads() {
+        assertNull(repository.findAll().blockFirst());
     }
 
 }
