c66c2e48a3ac46228cfdd226969c6e0cfd2e6137
Dmitry <koliasam1@gmail.com>
2025-03-03 19:39:23 +0300

    Add tests to service

--- src/test/java/ru/pet/nzcheinenm/service/ProductServiceTest.java
+++ src/test/java/ru/pet/nzcheinenm/service/ProductServiceTest.java
@@ -1,6 +1,36 @@
 package ru.pet.nzcheinenm.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
 import ru.pet.nzcheinenm.BaseTest;
+import ru.pet.nzcheinenm.dto.request.ProductRequestDto;
+import ru.pet.nzcheinenm.dto.response.ProductResponseDto;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ProductServiceTest  extends BaseTest {
+
+    @Autowired
+    ProductService productService;
+
+    @Test
+    public void saveProductTest() {
+        ProductRequestDto requestDto = ProductRequestDto.builder()
+                .externalId(Constants.EXTERNAL_ID)
+                .type(Constants.TYPE)
+                .status(Constants.STATUS)
+                .build();
+
+        Mono<ProductResponseDto> result = productService.saveProduct(requestDto);
+        assertNotNull(result.block());
+    }
+
+    private static class Constants {
+        public static final String EXTERNAL_ID = UUID.randomUUID().toString();
+        public static final String TYPE = "product_test";
+        public static final String STATUS = "CREATED";
+    }
 }
