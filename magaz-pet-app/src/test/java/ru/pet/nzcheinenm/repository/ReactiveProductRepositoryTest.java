d32d3adccbc66ae7922d9b446dfb8127f5f5d0cf
Dmitry <koliasam1@gmail.com>
2025-09-15 20:41:06 +0300

    add tests

--- magaz-pet-app/src/test/java/ru/pet/nzcheinenm/repository/ReactiveProductRepositoryTest.java
+++ magaz-pet-app/src/test/java/ru/pet/nzcheinenm/repository/ReactiveProductRepositoryTest.java
@@ -2,21 +2,21 @@ package ru.pet.nzcheinenm.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
 import ru.pet.nzcheinenm.BaseTest;
 import ru.pet.nzcheinenm.entity.Product;
 import types.StatusType;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReactiveProductRepositoryTest extends BaseTest {
     @Autowired
     ReactiveProductRepository repository;
 
     @Test
-    void smokeTest() {
+    void save_product_success() {
         assertNull(repository.findAll().blockFirst());
 
         Product product = new Product();
@@ -27,5 +27,22 @@ public class ReactiveProductRepositoryTest extends BaseTest {
         product.setCreatedDate(LocalDateTime.now());
 
         assertNotNull(repository.save(product).block());
+    }
+
+    @Test
+    void find_product_success() {
+
+        Product product = new Product();
+        product.setId("1234");
+        product.setType("Product");
+        product.setStatus(StatusType.COMPLETED);
+        product.setExternalId("4321");
+        product.setCreatedDate(LocalDateTime.now());
+
+        assertNotNull(repository.save(product).block());
+
+        Flux<Product> result = repository.findAllByStatus(StatusType.COMPLETED.name());
+        assertNotNull(result.blockFirst());
+        assertEquals(result.blockFirst(),product);
     }
 }
